590e5fa030ce970bddbbc0adf839f13a22aaf198
Uriy Efremochkin <333peps@gmail.com>
2013-01-06 01:22:07 +0600

    Simple feature "Installation project" has been added. Refactoring in PHP.

--- core/admin.php
+++ core/admin.php
@@ -10,7 +10,7 @@ class admin{
 
 	//==================================================================================================
 	// Функция вывода админки
-	function draw($tpl,$replace=''){
+	function draw($tpl, $replace=''){
 
 		global $find,$REPL;
 
@@ -19,20 +19,13 @@ class admin{
 
 		$REPL = $replace;
 
-		$filename = 'templates/'.$tpl.'.tpl';
-		// открываем файл шаблона
-		$handle = fopen($filename, "r");
-		// читаем файл шаблона
-		$tpl = fread($handle, filesize($filename));
-		// закрываем файл шаблона
-		fclose($handle);
-		// Если контент страницы задан как файл
-		if (isset($replace[$content]) and strpos($replace[$content], '.tpl')>-1){
-			$filename = 'templates/'.$content;
-			$handle = fopen($filename, "r");
-			$content = fread($handle, filesize($filename));
-			fclose($handle);
-		}
+        $tpl = 'templates/' . $tpl . '.tpl';
+        if (file_exists($tpl)) {
+            $tpl = file_get_contents($tpl);
+        } else {
+            die('error:draw-tpl(' . $tpl . ')');
+        }
+
 		// Если JavaScript задан как файл
 		if (isset($replace['js']) and strpos($replace['js'], '.js')>-1){
 			$filename = 'templates/'.$replace['js'];
@@ -160,26 +153,20 @@ class admin{
 	/**************************************************************************************************/
 	//==================================================================================================
 	// функция создания файла конфигурации
-	function createConfigSCR($POST){
-		$filename = '../config.php';
-		magic_quotes_runtime == true;
-		$content = '<?php
-		$CONF["db_name"] = "'.$POST['db_name'].'";
-		$CONF["db_host"] = "'.$POST['db_host'].'";
-		$CONF["db_login"] = "'.$POST['db_login'].'";
-		$CONF["db_pass"] = "'.$POST['db_password'].'";
-		$CONF["db_prefix"] = "'.$POST['db_prefix'].'";
-		?&gt;';
+	function createConfigSCR ($POST) {
+
+		$content = "<?php"
+		    . "\n\$CONF['db_name']   = '{$POST['db_name']}';"
+            . "\n\$CONF['db_host']   = '{$POST['db_host']}';"
+            . "\n\$CONF['db_login']  = '{$POST['db_login']}';"
+            . "\n\$CONF['db_pass']   = '{$POST['db_password']}';"
+            . "\n\$CONF['db_prefix'] = '{$POST['db_prefix']}';";
 
 		$content = html_entity_decode($content);
 
-	    if (!$handle = fopen($filename, 'w')) {
-			exit;
-	    }
-		if (fwrite($handle, $content) === FALSE) {
-			exit;
+	    if (!file_put_contents('../config.php', $content)) {
+			die('error: config-file-write');
 		}
-		fclose($handle);
 
 		admin::createFolders();
 	}
@@ -191,37 +178,30 @@ class admin{
 
 	//==================================================================================================
 	// функция создания папок
-	function createFolders(){
+	function createFolders() {
+
 		if (!file_exists('../site')) {
 			mkdir('../site', 0777);
 		}
-
 		if (!file_exists('../site/templates')) {
 			mkdir('../site/templates', 0777);
 		}
-
 		if (!file_exists('../site/images')) {
 			mkdir('../site/images', 0777);
 		}
-
 		if (!file_exists('../data')) {
 			mkdir('../data', 0777);
 		}
-
 		if (!file_exists('../data/files')) {
 			mkdir('../data/files', 0777);
 		}
-
 		if (!file_exists('../data/images')) {
 			mkdir('../data/images', 0777);
 		}
-
 		if (!file_exists('../data/wysiwyg')) {
 			mkdir('../data/wysiwyg', 0777);
 		}
-
 		admin::databaseWrite();
-
 	}
 	// функция создания папок
 	//==================================================================================================
@@ -231,10 +211,9 @@ class admin{
 
 	//==================================================================================================
 	// функция создания таблиц и записей
-	function databaseWrite(){
+	function databaseWrite() {
 
 		global $CONF;
-
 		$CONF = sys::preLoad();
 
 		// Создание таблиц для модулей
@@ -242,55 +221,30 @@ class admin{
 		modules_structure_admin::createTable();
 
 		$modules = scandir('../core/modules');
-		
-		foreach ($modules as $value){
-
-			if ($value != '.' and $value != '..'){
-
-				$file = explode(".",$value);
-				$module = explode("_",$file[0]);
-
-				if ($module[1]=='admin'){
-
-					if ($module[0]!='structure' and $module[0]!='baseclass'){
-
-						eval('$class_methods = get_class_methods(modules_'.$module[0].'_admin);');
-
-						if (array_search('createTable', $class_methods)){
-							eval('modules_'.$module[0].'_admin::createTable();');
-						}
-
-					}
-
+		foreach ($modules as $module) {
+			if ($module[0] != '.') {
+				$module = basename($module, '.php');
+				if ($module != 'structure_admin' && $module != 'baseclass_admin' && substr($module, -6) == '_admin') {
+                    $module = 'modules_' . $module;
+                    if (method_exists($module, 'createTable')) {
+                        call_user_func(array($module, 'createTable'));
+                    }
 				}
-
 			}
-
 		}
-		// Создание таблиц для модулей
-
 
 		// Создание таблиц для компонентов
-		$modules = scandir('../core/components');
-		
-		foreach ($modules as $value){
-
-			if ($value != '.' and $value != '..'){
-
-				$file = explode(".",$value);
-				eval('$class_methods = get_class_methods(components_'.$file[0].');');
-
-				if (array_search('createTable', $class_methods)){
-					eval('components_'.$file[0].'::createTable();');
-				}
-
+		$components = scandir('../core/components');
+		foreach ($components as $component) {
+			if ($component[0] != '.') {
+                $component = 'components_' . basename($component, '.php');
+                if (method_exists($component, 'createTable')) {
+                    call_user_func(array($component, 'createTable'));
+                }
 			}
-
 		}
-		// Создание таблиц для компонентов
 
 		admin::createAdmin();
-
 	}
 	// функция создания таблиц и записей
 	//==================================================================================================
@@ -300,34 +254,39 @@ class admin{
 
 	//==================================================================================================
 	// функция создания админа
-	function createAdmin(){
+	function createAdmin() {
+
 		// Создаём таблицу групп
-		$sql = sys::sql("CREATE TABLE IF NOT EXISTS `prefix_groups` (
-							`name` varchar(255) NOT NULL,
-							`title` varchar(255) NOT NULL,
-							`description` text NOT NULL,
-							`policy` text NOT NULL,
-							PRIMARY KEY (`name`)
-						) ENGINE=MyISAM DEFAULT CHARSET=utf8
-		;",0);
-		// Создаём таблицу политик
-		$sql = sys::sql("CREATE TABLE IF NOT EXISTS `prefix_policy` (
-							`name` varchar(255) NOT NULL,
-							`description` text NOT NULL,
-							PRIMARY KEY (`name`)
-						) ENGINE=MyISAM DEFAULT CHARSET=utf8
-		;",0);
-		
-		$sql = sys::sql("SELECT
-							*
-						FROM
-							`prefix_groups`
-						WHERE
-							`name` = 'root'
+		sys::sql("
+            CREATE TABLE IF NOT EXISTS `prefix_groups` (
+                `name` varchar(255) NOT NULL,
+                `title` varchar(255) NOT NULL,
+                `description` text NOT NULL,
+                `policy` text NOT NULL,
+                PRIMARY KEY (`name`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8
 		;",0);
 
-		if (mysql_num_rows($sql)==0){
-			$sql = sys::sql("
+		// Создаём таблицу политик
+		sys::sql("
+            CREATE TABLE IF NOT EXISTS `prefix_policy` (
+                `name` varchar(255) NOT NULL,
+                `description` text NOT NULL,
+                PRIMARY KEY (`name`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8
+		;",0);
+		
+		$sql = sys::sql("
+            SELECT
+                *
+            FROM
+                `prefix_groups`
+            WHERE
+                `name` = 'root'
+		;",0);
+
+		if (mysql_num_rows($sql) == 0) {
+			sys::sql("
 				INSERT INTO
 					`prefix_groups`
 					(
@@ -346,51 +305,50 @@ class admin{
 			;",0);
 		}
 
-		$sql = sys::sql("SELECT
-							`id`
-						FROM
-							`prefix_ClassSections`
-						WHERE
-							`name` = 'users' AND
-							`type` = 'users'
+		$sql = sys::sql("
+            SELECT
+                `id`
+            FROM
+                `prefix_ClassSections`
+            WHERE
+                `name` = 'users' AND
+                `type` = 'users'
 		;",0);
 
 		if (mysql_num_rows($sql)==0){
-
-			$sql = sys::sql("INSERT INTO
-								`prefix_ClassSections`
-							VALUES (
-								'',
-								'0',
-								'users',
-								'Пользователи',
-								'users',
-								''
-							)
+            sys::sql("
+                INSERT INTO
+                    `prefix_ClassSections`
+                VALUES (
+                    '',
+                    '0',
+                    'users',
+                    'Пользователи',
+                    'users',
+                    ''
+                )
 			;",0);
 
-			$id = mysql_insert_id();
-			$id_root = $id;
+            $id_root = $id = mysql_insert_id();
 
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'user', 'Пользователь', 'type_children', '');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'user', 'Пользователь', 'type_children', '');",0);
 
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', 0, 'user', 'Пользователь', 'type', '');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', 0, 'user', 'Пользователь', 'type', '');",0);
 			$id = mysql_insert_id();
 			$SEND['baseclass'] = mysql_insert_id();
 
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'password', 'Пароль', 'attr', 'TVarchar');",0);
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'group', 'Группа', 'attr', 'TVarchar');",0);
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'mail', 'E-mail', 'attr', 'TVarchar');",0);
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'active', 'Активность', 'attr', 'TBoolev');",0);
-			$sql = sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'code', 'Код', 'attr', 'TVarchar');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'password', 'Пароль', 'attr', 'TVarchar');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'group', 'Группа', 'attr', 'TVarchar');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'mail', 'E-mail', 'attr', 'TVarchar');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'active', 'Активность', 'attr', 'TBoolev');",0);
+			sys::sql("INSERT INTO `prefix_ClassSections` VALUES ('', '$id', 'code', 'Код', 'attr', 'TVarchar');",0);
 
-			$sql = sys::sql("INSERT INTO `prefix_Sections` VALUES ('', '0', '0', 'users', 'Пользователи', '$id_root', '');",0);
+			sys::sql("INSERT INTO `prefix_Sections` VALUES ('', '0', '0', 'users', 'Пользователи', '$id_root', '');",0);
 
 			$SEND['parent_id'] = mysql_insert_id();
 		}
 
-		echo admin::draw('_install/step4',$SEND);
-	
+		echo admin::draw('_install/step4', $SEND);
 	}
 	// функция создания админа
 	//==================================================================================================
@@ -462,12 +420,13 @@ class admin{
 	// Ввод ключа для CMS
 	function enterKey($POST){
 
-		$sql = sys::sql("CREATE TABLE
-							`prefix_key` (
-								`key` text NOT NULL
-							)
-							ENGINE=MyISAM
-							CHARACTER SET utf8 COLLATE utf8_general_ci
+		sys::sql("
+            CREATE TABLE IF NOT EXISTS
+            `prefix_key` (
+                `key` text NOT NULL
+            )
+            ENGINE=MyISAM
+            CHARACTER SET utf8 COLLATE utf8_general_ci
 		;",0);
 		
 		$sql = sys::sql("INSERT INTO
@@ -492,33 +451,33 @@ class admin{
 
 	//==================================================================================================
 	//Функция проверки ключа
-	function keyValid(){
+	function keyValid() {
 
-		$sql = sys::sql("SELECT
-							`key`
-						FROM
-							`prefix_key`
-						LIMIT 1
-		;",0);
+		$sql = sys::sql('
+            SELECT
+                `key`
+            FROM
+                `prefix_key`
+            LIMIT 1
+		;',0);
 
 		$original = mysql_result($sql,0);
 
-		$to=$_SERVER[HTTP_HOST];
-		$to = str_replace("www.","",$to);
+		$to = $_SERVER['HTTP_HOST'];
+		$to = str_replace('www.', '', $to);
 
-		for ($x=0; $x<strlen($to);$x++){
+		for ($x = 0; $x < strlen($to); $x++) {
 			$znak[$x] =(int) (((ord($to[$x]) + 341)*17)/7);
 			$out = $out + $znak[$x];
 		}
 
 		$out = md5($out);	
 
-		if ($out == $original){
+		if ($out == $original) {
 			return true;
-		}else{
+		} else {
 			return true;
 		}
-
 	}
 	//Функция проверки ключа
 	//==================================================================================================
@@ -537,19 +496,19 @@ session_start();
 // Загрузка CONFIG.php
 $CONF = sys::preLoad();
 
-if (count($CONF)==0 or $_POST['action']=='createCONF'){
+if (empty($CONF) or (isset($_POST['parametr']) && isset($_POST['act']) && $_POST['act'] == 'install')) {
 
-	if (isset($_POST['parametr']) and $_POST['parametr']=='step1'){
-		admin::createConfigSCR($_POST);
-	}elseif (isset($_POST['parametr']) and $_POST['parametr']=='step4'){
+	if ($_POST['parametr'] == 'step1') {
+		echo 1;
+        admin::createConfigSCR($_POST);
+	}elseif ($_POST['parametr'] == 'step4') {
 		admin::createAdmin2($_POST);
-	}elseif (isset($_POST['parametr']) and $_POST['parametr']=='step5'){
+	}elseif ($_POST['parametr'] == 'step5') {
 		admin::enterKey($_POST);
 	}else{
 		echo admin::draw('_install/step1');
 	}
-
-}else{
+} else {
 
 	if (admin::keyValid()){
 		if (modules_users_sys::getPolicy('admin_access')){
@@ -577,5 +536,3 @@ $_SESSION['cur_number'] = 1096;
 	}
 
 }
-
-?>
--- core/modules/baseclass_admin.php
+++ core/modules/baseclass_admin.php
@@ -601,47 +601,47 @@ class modules_baseclass_admin{
 	// Функция создания таблиц
 	function createTable(){
 
-		$sql=sys::sql("CREATE TABLE IF NOT EXISTS
-						`prefix_ClassSections` (
-							`id` int(11) NOT NULL auto_increment,
-							`parent_id` int(11) NOT NULL,
-							`name` varchar(255) NOT NULL,
-							`title` varchar(255) NOT NULL,
-							`type` varchar(255) NOT NULL,
-							`value` varchar(255) NOT NULL,
-							PRIMARY KEY  (`id`)
-						)
-						ENGINE=MyISAM
-						CHARACTER SET utf8 COLLATE utf8_general_ci
-						COMMENT='Таблица Базовых Типов'
-						AUTO_INCREMENT=1
+		sys::sql("
+            CREATE TABLE IF NOT EXISTS
+            `prefix_ClassSections` (
+                `id` int(11) NOT NULL auto_increment,
+                `parent_id` int(11) NOT NULL,
+                `name` varchar(255) NOT NULL,
+                `title` varchar(255) NOT NULL,
+                `type` varchar(255) NOT NULL,
+                `value` varchar(255) NOT NULL,
+                PRIMARY KEY  (`id`)
+            )
+            ENGINE=MyISAM
+            CHARACTER SET utf8 COLLATE utf8_general_ci
+            COMMENT='Таблица Базовых Типов'
+            AUTO_INCREMENT=1
 		;",0);
 
-		$sql = sys::sql("SELECT
-							`name`
-						FROM
-							`prefix_ClassSections`
-						WHERE
-							`id`='1'
+		$sql = sys::sql("
+            SELECT
+                `name`
+            FROM
+                `prefix_ClassSections`
+            WHERE
+                `id`='1'
 		;",0);
 
-		if (mysql_num_rows($sql)==0){    
-			$sql=sys::sql("INSERT INTO
-								`prefix_ClassSections`
-							VALUES(
-								'1',
-								'0',
-								'root',
-								'Корень',
-								'type',
-								''
-							)
+		if (mysql_num_rows($sql) == 0) {
+			sys::sql("
+                INSERT INTO
+                    `prefix_ClassSections`
+                VALUES(
+                    '1',
+                    '0',
+                    'root',
+                    'Корень',
+                    'type',
+                    ''
+                )
 			;",0);
 		}
-
 	}
 	// Функция создания таблиц
 	//==================================================================================================
 }
-// Конец класса
-?>
