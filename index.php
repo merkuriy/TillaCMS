277cabef85b1aaa20b1e3bc418ddf965dacd2c2b
Uriy Efremochkin <333peps@gmail.com>
2013-09-15 21:31:59 +0600

    Improved work with index.php and refactoring from htaccess.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
+<?php header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500); ?>
 <h1>This site is temporarily not working</h1>
 <p>index.php</p>
 <?php
-if( isset($_GET['info']) ) phpinfo();
-die("+php work+");
-?>
+if (isset($_GET['info'])) phpinfo();
+die('php work');
