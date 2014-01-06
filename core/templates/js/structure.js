a0d9da489e6b016c879f29c144aa25d35bbfc4a7
Uriy Efremochkin <333peps@gmail.com>
2014-01-06 19:18:19 +0600

    Tilla.TinyMCE JS module has been created. Code-style refactoring.

--- core/templates/js/structure.js
+++ core/templates/js/structure.js
@@ -60,13 +60,16 @@ window.Structure || (Structure = (function() {
       success: function(data) {
         $.ajax({
           url: "/api.post/structure_panel.draw_sub?tpl=content/edit",
-          success: function(tpl) {
+          success: function (tpl) {
+
             $('#node-content').html($('<div>'+tpl+'</div>').tmpl(data));
             $('#content-zone').css('height', $(window).height()-168);
             $('.fieldLabel').tooltip();
 
-            $.each(data.attrs, function() {
-              var url  = this.component,
+            $.each(data.attrs, function () {
+
+              var
+                url  = this.component,
                 data = this;
 
               if (this.type) {
@@ -80,19 +83,19 @@ window.Structure || (Structure = (function() {
                 $('#components select').each(function() {
                   $(this).find('option[value="'+$(this).data('value')+'"]').attr('selected', 'selected');
                 });
-              }
-              if (this.component == 'TBoolev') {
+
+              } else if (this.component == 'TBoolev') {
                 $('#field-'+this.name).find('[data-value="'+this.value+'"]').addClass('active');
-              }
-              if (this.component == 'TDate') {
+
+              } else if (this.component == 'TDate') {
                 $('#field-'+this.name).datetimepicker({
                   showSecond: true,
                   timeFormat: 'hh:mm:ss',
                   dateFormat: 'dd.mm.yy'
                 });
                 $('#field-'+this.name).datetimepicker('setDate', new Date(this.value));
-              }
-              if (this.component == 'TMultiUpLoad') {
+
+              } else if (this.component == 'TMultiUpLoad') {
                 $('#field-'+this.name).pluploadQueue({
                   // General settings
                   runtimes : 'gears,flash,silverlight,browserplus,html5',
@@ -111,9 +114,9 @@ window.Structure || (Structure = (function() {
                   // Silverlight settings
                   silverlight_xap_url : '/core/templates/js/plupload/plupload.silverlight.xap'
                 });
-              }
-              if (this.component == 'TText') {
-                if (this.type == '0') {
+
+              } else if (this.component == 'TText') {
+                if (this.type == 0) {
                   $('#field-'+this.name).redactor({
                     lang:         'ru',
                     autoresize:   false,
@@ -122,55 +125,16 @@ window.Structure || (Structure = (function() {
                     fileUpload:   '/api.post/structure_panel.file_upload',
                     plugins:      ['fullscreen']
                   });
-                }
-                if (this.type == '3') {
-                  $('#field-'+this.name).tinymce({
-                    // Location of TinyMCE script
-                    script_url : '/core/templates/js/tiny_mce/tiny_mce.js',
 
-                    // General options
-                    theme : "advanced",
-                    language : "ru",
-                    relative_urls : false,
-                    plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,images",
-                    file_browser_callback : "upload",
-
-                    // Theme options
-                    theme_advanced_buttons1 : "mylistbox,mysplitbutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink,|,fullscreen",
-                    theme_advanced_buttons2 : "",
-                    theme_advanced_buttons3 : "",
-                    fullscreen_settings : {
-                      theme_advanced_buttons1 : "save,|,undo,redo,|,cut,copy,paste,pastetext,pasteword,|,search,replace,|,cleanup,removeformat,visualaid,visualchars,nonbreaking,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent,indent,blockquote,|,sub,sup,|,bullist,numlist,formatselect,code,|,fullscreen",
-                      theme_advanced_buttons2 : "tablecontrols,|,insertlayer,moveforward,movebackward,absolute,|,link,unlink,anchor,|,insertdate,inserttime,|,charmap,emotions,iespell,media,image,images,advhr,hr,|,cite,abbr,acronym,del,ins,attribs,styleprops,|,typograf",
-                      theme_advanced_path_location : "bottom"
-                    },
-                    theme_advanced_toolbar_location : "top",
-                    theme_advanced_toolbar_align : "left",
-                    theme_advanced_statusbar_location : "bottom",
-                    theme_advanced_resizing : true,
-
-                    setup : function(ed){
-                      if (ed.editorId != 'mce_fullscreen') {
-                        tinyMCE.myActiveEditor = {};
-                        tinyMCE.myActiveEditor = ed;
-                      }
-                      ed.onActivate.add( function(ed){
-                        if( ed.editorId != 'mce_fullscreen' ){
-                          tinyMCE.myActiveEditor = {};
-                          tinyMCE.myActiveEditor = ed;
-                        }
-                      });
-                      ed.onLoadContent.add( function(ed){
-                        ed.addCommand('mceSave', function(){
-                          $('#content-save').click();
-                          return false;
-                        });
-                      });
-                    }
+                } else if (this.type == 3) {
+                  Tilla.TinyMCE.render('#field-'+this.name, function () {
+                    $('#content-save').click();
+                    return false;
                   });
                 }
               }
             });
+
             window.location.hash = sectionId;
           }
         });
--- core/templates/js/tilla.js
+++ core/templates/js/tilla.js
@@ -146,6 +146,73 @@ var $load_settings = function() {
 }
 
 
+
+Tilla = {};
+/**
+ * Tilla Text Editor TinyMCE
+ */
+Tilla.TinyMCE = (function () {
+
+  return {
+    render: initTinyMCE
+  }
+
+
+  function initTinyMCE (selector, saveCallback) {
+
+    $(selector).tinymce({
+      // Location of TinyMCE script
+      script_url: '/core/templates/js/tiny_mce/tiny_mce.js',
+
+      // General options
+      theme: 'advanced',
+      language : "ru",
+      relative_urls : false,
+      plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,images",
+      file_browser_callback : "upload",
+
+      // Theme options
+      theme_advanced_buttons1 : "mylistbox,mysplitbutton,bold,italic,underline,separator,strikethrough,justifyleft,justifycenter,justifyright,justifyfull,bullist,numlist,undo,redo,link,unlink,|,fullscreen",
+      theme_advanced_buttons2 : "",
+      theme_advanced_buttons3 : "",
+      fullscreen_settings : {
+        theme_advanced_buttons1 : "save,|,undo,redo,|,cut,copy,paste,pastetext,pasteword,|,search,replace,|,cleanup,removeformat,visualaid,visualchars,nonbreaking,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,outdent,indent,blockquote,|,sub,sup,|,bullist,numlist,formatselect,code,|,fullscreen",
+        theme_advanced_buttons2 : "tablecontrols,|,insertlayer,moveforward,movebackward,absolute,|,link,unlink,anchor,|,insertdate,inserttime,|,charmap,emotions,iespell,media,image,images,advhr,hr,|,cite,abbr,acronym,del,ins,attribs,styleprops,|,typograf",
+        theme_advanced_path_location : "bottom"
+      },
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      theme_advanced_resizing : true,
+
+      setup: setup
+    });
+
+    function setup (ed) {
+
+      fullscreenFix(ed);
+      ed.onActivate.add(fullscreenFix);
+      ed.onLoadContent.add(onLoadContent);
+    }
+
+    function onLoadContent (ed) {
+      ed.addCommand('mceSave', saveCallback);
+    }
+  }
+
+  function fullscreenFix (ed) {
+
+    if (ed.editorId != 'mce_fullscreen') {
+      tinyMCE.myActiveEditor = {};
+      tinyMCE.myActiveEditor = ed;
+    }
+  }
+
+})();
+
+
+
+
 $().ready(function() {
 
   /**
